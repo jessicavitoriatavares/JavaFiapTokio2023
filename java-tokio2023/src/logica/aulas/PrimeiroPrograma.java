ddff9f52369e8b8de66f779180b60c4e2d00e38d
jessicavitoriatavares <jessica.vitoria.tavares044@gmail.com>
2023-04-13 18:25:01 -0300

    primeiro commit java

---
+++ java-tokio2023/src/logica/aulas/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package logica.aulas;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		System.out.println("Ola, mundo");
+		
+		int idade; 
+		
+		idade = 18;
+		System.out.println(idade);
+		
+		idade = 42;
+		System.out.println(idade);
+		
+		double salario = 1318.20;
+		System.out.println(salario);
+		
+		String nome = "Jessica";
+		System.out.println(nome);
+		
+		System.out.println("Seja bem-vinda," + nome + "!");
+		System.out.println(nome + ", Voce tem " + idade + " anos");
+	}
+
+}
---
+++ java-tokio2023/src/logica/aulas/SegundoPrograma.java
@@ -0,0 +1,21 @@
+package logica.aulas;
+import java.util.Scanner;
+
+public class SegundoPrograma {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite seu nome");
+		String nome = entrada.nextLine();
+		System.out.println("Bem-vindo, " + nome);
+		
+		System.out.println("Digite sua idade");
+		System.out.println("sua idade eh:, " + entrada.nextLine());
+		
+		
+
+	}
+
+}
